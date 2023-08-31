591489ae39686714022c8442b82e1e481247fb3d
anoushka-sahay <anoushkas20@iitk.ac.in>
2023-08-31 17:32:23 +0800

    trained model artifacts

--- crown-plaza/labelstudio.sh
+++ crown-plaza/labelstudio.sh
@@ -1,3 +1,3 @@
 echo "Running Label Studio in the background"
-docker run -it --user root --name labelstudio -p 5000:8080 -v $(pwd)/labelstudio-data:/label-studio/data heartexlabs/label-studio:latest 
+docker run -dit --user root --name labelstudio -p 5000:8080 -v $(pwd)/data:/label-studio/data -e LABEL_STUDIO_LOCAL_FILES_SERVING_ENABLED=true -e LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT=/label-studio/data heartexlabs/label-studio:latest 
 # chown -R 1001:root /label-studio/data/
